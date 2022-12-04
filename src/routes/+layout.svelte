17f6c26626968b6ee0c8c706357dd51cf4fb90df
Robert56s <78372422+Robert56s@users.noreply.github.com>
2022-12-03 20:25:10 -0500

    big update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,100 @@
+<script>
+</script>
+
+<svelte:head>
+    <style>
+		@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+
+		* {
+			padding: 0;
+			margin: 0;
+			font-family: 'Roboto', sans-serif;
+			scrollbar-width: none;
+			letter-spacing: 0.5px;
+		}
+
+		body {
+			background: #1f1f1f;
+		}
+
+		::-webkit-scrollbar {
+				width: 0;
+		}
+		
+	</style>
+</svelte:head>
+
+<div class="app">
+    <header>
+        <a href="/"><img src="https://cdn.discordapp.com/attachments/756373435726168104/769299914332962886/krnlmain.png" alt=""></a>
+        <nav>
+            <div class="ul">
+                <a href="/">Download</a>
+                <a href="/doc">Documentation</a>
+                <a href="https://discord.com/invite/6AGKKRaT4z">Discord</a>
+                <a href="/faq">Faq</a>
+            </div>
+        </nav>
+    </header>
+    
+    <slot/>
+    <footer>
+        Copyright © Krnl 2022
+    </footer>
+    
+</div>
+
+<style>
+
+    .app {
+        color: white;
+    }
+    header {
+        position: sticky;
+        top: 0;
+        display: flex;
+        padding-inline: 2rem;
+        justify-content: space-between;
+        align-items: center;
+        color: white;
+        height: 10vh;
+        background: #000000b2;
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+    }
+
+    
+    img {
+        width: 64px;
+        height: 60px;
+        margin-left: 2rem;
+    }
+
+    .ul {
+        display: flex;
+    }
+
+    nav a {
+        padding: 1rem 2rem;
+        margin: 1rem;
+        background: #004974;
+        border-radius: 10px;
+        transition: 0.3s;
+    }
+
+    nav a:hover {
+        background: #006494;
+    }
+
+    nav a:active {
+        transform: scale(0.9);
+    }
+
+
+    footer {
+        text-align: center;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,96 @@
+<div class="first">
+    <div class="group1">
+        <h1>Krnl</h1>
+        <p>Krnl is a scripting utility with an intuitive graphical interface to help you quickly write and execute scripts.</p>
+    </div>
+</div>
+
+<div class="second">
+    <h1>Features</h1>
+    <div class="group2">
+        <div class="box">
+            <h3>Stable</h3>
+            <p>By applying a variety of technology, Krnl provides a seamless user experience.</p>
+        </div>
+        <div class="box">
+            <h3>Performant</h3>
+            <p>Responsive execution times are achieved through the implementation of LuaU.</p>
+        </div>
+        <div class="box">
+            <h3>Reliable</h3>
+            <p>With exceptional update times, Krnl boasts near perfect uptime.</p>
+        </div>
+    </div>
+</div>
+
+<div class="third">
+    <h1>Credits</h1>
+    <div class="contributors">
+        <div class="devbox"></div>
+        <div class="devbox"></div>
+        <div class="devbox"></div>
+        <div class="devbox"></div>
+        <div class="devbox"></div>
+    </div>
+</div>
+
+
+<style>
+
+    h1 {
+        padding-top: 1rem;
+    }
+    
+    p {
+        padding: 1rem;
+        color: rgb(153, 153, 153);
+    }
+
+    .first {
+        margin-top: 5rem;
+        justify-content: center;
+        padding: 1rem;
+        text-align: center;
+    }
+
+    .first .group1 {
+        display: inline-block;
+        max-width: 30rem;
+        border-style: solid;
+        border-radius: 5px;
+        border-color: rgb(89, 48, 128);
+        border-width: 5px;
+    }
+
+    .second h1 {
+        text-align: center;
+        padding-bottom: 4rem;
+    }
+
+    .second {
+        margin-top: 5rem;
+        margin-inline: 13rem;
+        
+    }
+
+    .group2 {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 15rem;
+        
+    }
+
+    .box {
+        padding: 1rem;
+        margin-inline: 3rem;
+        border-style: solid;
+        border-radius: 5px;
+        border-color: rgb(89, 48, 128);
+        border-width: 5px;
+        flex: 1
+    }
+
+    .box h3 {
+        padding: 1rem;
+    }
+</style>
